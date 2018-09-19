3b6c6db4e8e223eb3cbebb9e903cd2fcd038a0dd
lrlineroa <leonardoraulinero@gmail.com>
2018-09-19 12:51:34 -0500

    se puso constraint default false a la migracion create users lists

--- db/migrate/20180824175332_create_users_lists.rb
+++ db/migrate/20180824175332_create_users_lists.rb
@@ -3,9 +3,9 @@ class CreateUsersLists < ActiveRecord::Migration[5.0]
     create_table :users_lists do |t|
       t.references :list, foreign_key: true, null: false
       t.references :user, foreign_key: true, null: false
-      t.boolean :is_owner, null: false, default: 0
-      t.boolean :visible, null: false, default: 0
-      t.boolean :can_edit, null: false, default: 0
+      t.boolean :is_owner, null: false, default: false
+      t.boolean :visible, null: false, default: false
+      t.boolean :can_edit, null: false, default: false
 
       t.timestamps
     end
