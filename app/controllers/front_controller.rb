342377374fa7532b6edb38867b871e045f647789
lrlineroa <leonardoraulinero@gmail.com>
2018-09-19 17:14:51 -0500

    se adiciono un placeholder

--- app/controllers/front_controller.rb
+++ app/controllers/front_controller.rb
@@ -1,5 +1,5 @@
 class FrontController < ApplicationController
 	def index
-		render plain: 'Este es el api de todo list por favor no intentar violar la seguridad, por que estamos protegidos por JWT'
+		render plain: 'Este es el api de todo list por favor no intentar violar la seguridad, por que estamos protegidos por JWT Si quieres saber como utilizar este api te recomiendo la guia https://docs.google.com/document/d/1PZesKGdQlE5KtyVVDBteHJhoWdHxq8v-nCrTbDGGXBI/edit?usp=sharing'
 	end
 end
